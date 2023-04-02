295f2ae04e7548356890069f25251d405b17f17b
Michael Wagner <125706689+MichaelWagner21@users.noreply.github.com>
2023-04-01 20:23:55 -0600

    commit

--- Primes.py
+++ Primes.py
@@ -1,19 +1,26 @@
+import numpy as np
+
 import time
 start_time = time.time()
 numOfPrimes = 100000
 numsPrinted = 0
 numberIterator = 2
+recordedPrimes = np.zeros(numOfPrimes+1)
+
 composite = False
 while numsPrinted<numOfPrimes:
+    celingCheck = numberIterator**0.5
     composite = False
-    i = 2
-    while i<numberIterator:
-        if i!=numberIterator:
-            if numberIterator%i==0:
-                composite = True
+
+    i = 0
+    while recordedPrimes[i]<=celingCheck and recordedPrimes[i]!=0:
+        if numberIterator%recordedPrimes[i]==0:
+            composite = True
         i+=1
     if not composite:
         print(numberIterator)
+        recordedPrimes[numsPrinted]=numberIterator
         numsPrinted+=1
     numberIterator+=1
-print("--- %s seconds ---" % (time.time()-start_time))
+end_time = time.time()
+print("--- %s seconds ---" % (end_time-start_time))
